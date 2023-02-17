4c093ab3039c93008e9ebdf1e8b4252dd1999006
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-02-18 00:46:21 +0530

    Add files via upload

---
+++ day4-linkedlist2/middleoflinkedlist.py
@@ -0,0 +1,27 @@
+'''
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=None
+        while head:
+            curr=head
+            head=head.next
+            curr.next=prev
+            prev=curr
+        return prev
+
+#most important program as slow and fast are used regularly
---
+++ test.py
@@ -0,0 +1,3 @@
+strs = ["flower","flow","flight"]
+lens=map(len,strs)
+print(lens)
