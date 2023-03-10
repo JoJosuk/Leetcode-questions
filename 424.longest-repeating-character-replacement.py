44978e2f32cc56976879704fa9871acc9c3838fa
Joel J George <91320759+JoJosuk@users.noreply.github.com>
2023-03-10 10:14:35 +0530

    2sum11

---
+++ 424.longest-repeating-character-replacement.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=424 lang=python3
+#
+# [424] Longest Repeating Character Replacement
+#
+
+# @lc code=start
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        maxf = i = 0
+        count = collections.Counter()
+        for j in range(len(s)):
+            count[s[j]] += 1
+            maxf = max(maxf, count[s[j]])
+            if j - i + 1 > maxf + k:
+                count[s[i]] -= 1
+                i += 1
+        return len(s) - i
+# @lc code=end
+
